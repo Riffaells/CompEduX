33ca74a97a2970a943fdb0775ecf15d729dba9aa
Riffaells <riffaells@gmail.com>
2025-03-09 01:49:45 +0500

    update base structure

---
+++ core/common/src/iosMain/kotlin/utils/RDispatchers.kt
@@ -0,0 +1,16 @@
+package utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * iOS-специфичная реализация RDispatchers.
+ * Использует Dispatchers.Main для UI-операций на iOS.
+ */
+actual val rDispatchers: RDispatchers = object : RDispatchers {
+    // На iOS Dispatchers.Main доступен через kotlinx-coroutines-core
+    override val main: CoroutineDispatcher = Dispatchers.Main
+    override val io: CoroutineDispatcher = Dispatchers.Default // iOS не имеет отдельного IO диспетчера
+    override val unconfined: CoroutineDispatcher = Dispatchers.Unconfined
+    override val default: CoroutineDispatcher = Dispatchers.Default
+}
---
+++ core/common/src/wasmMain/kotlin/utils/RDispatchers.kt
@@ -0,0 +1,19 @@
+package utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * Wasm-специфичная реализация RDispatchers.
+ * В Wasm нет отдельного Main диспетчера, поэтому используем Default.
+ */
+actual val rDispatchers: RDispatchers = object : RDispatchers {
+    // В Wasm нет отдельного Main диспетчера, поэтому используем Default
+    override val main: CoroutineDispatcher = Dispatchers.Default
+
+    // В Wasm нет отдельного IO диспетчера, поэтому используем Default
+    override val io: CoroutineDispatcher = Dispatchers.Default
+
+    override val unconfined: CoroutineDispatcher = Dispatchers.Unconfined
+    override val default: CoroutineDispatcher = Dispatchers.Default
+}
